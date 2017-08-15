15e17618d4879f87073065afd95d9b78677fd042
Bob Brown <robertbernardbrown@gmail.com>
2017-08-14 22:12:04 -0400

    FizzBuzz practice
    Practicing if statements

---
+++ FizzBuzz.code.js
@@ -0,0 +1,11 @@
+for (var i = 1; i <= 100; i += 1) {
+ 
+ if (i % 3 === 0 && i % 5 !== 0)
+  {console.log ('fizz')}
+   if (i % 5 === 0 && i % 3 !== 0)
+  {console.log ('buzz')}
+    if (i % 3 === 0 && i % 5 === 0)
+  {console.log ('fizzbuzz')}
+    if (i % 3 !== 0 && i % 5 !== 0)
+  {console.log (i)}
+}
